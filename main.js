f1f38c6a1cd5d96a4e255cdd0dffd8988cf5d2ba
umeramin218 <160252422+umeramin218@users.noreply.github.com>
2024-05-01 11:18:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("Hello, World");
+console.log("hello, Umer Amin");
+console.log("Hi, I'm a programer");
---
+++ main.ts
@@ -0,0 +1,7 @@
+console.log("Hello, World");
+
+console.log("hello, Umer Amin");
+
+console.log("Hi, I'm a programer");
+
+
